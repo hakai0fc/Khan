0617e359ad075bae93c7df98ff3af496dc4e35f3
hakai0fc <hackerdesconhecido0001@gmail.com>
2025-10-09 09:27:58 -0300

    Update Hack.js

--- Hack.js
+++ Hack.js
@@ -1,5 +1,5 @@
 /* ===========================
-   Khan/Hakai - cleaned script
+   Khan/Hakai - Funcional Script
    =========================== */
 
 /* Globals */
@@ -262,7 +262,7 @@ function setupMain() {
           playAudio("https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/4x5g14gj.wav");
         }
       }
-      await delay(800);
+      await delay(785;
     }
   })();
 
